6fe4d10586302014930bb894878905ad0fb21800
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-11 01:23:46 +0530

    9-PPI29.65-24-773d3-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_doha.mk
+++ lineage_doha.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := moto g(8) plus
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 TARGET_VENDOR := motorola
 TARGET_VENDOR_PRODUCT_NAME := doha
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC=" 9 PPIS29.65-24-2 dc7f7b release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC=" 9 PPI29.65-24 773d3 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := 9/PPIS29.65-24-2/dc7f7b:user/release-keys
+BUILD_FINGERPRINT := 9/PPI29.65-24/773d3:user/release-keys
